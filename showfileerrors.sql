9f4640af586da282f4056dba24b9f6e1b768aa7a
datahai <70232001+datahai@users.noreply.github.com>
2021-12-03 16:42:34 +0000

    Add new SQL View for showing errors
    This SQL will create a view to show any errors logged when using the Serverless SQL Pools OPENROWSET error handling options ERRORFILE_LOCATION and ERRORFILE_DATA_SOURCE.

---
+++ showfileerrors.sql
@@ -0,0 +1,28 @@
+CREATE VIEW dbo.ShowFileErrors
+AS
+SELECT
+    [Error],
+    [Row],
+    [Column],
+    ColumnName,
+    Value,
+    [File],
+    REVERSE(SUBSTRING(REVERSE([File]),
+            2,
+            CHARINDEX('/',REVERSE([File]))-2)) AS ErrorFileName,
+    rowdata.filepath(1) AS ErrorFolderName
+FROM OPENROWSET(
+        BULK '<path_to_your_error_folder>/_rejectedrows/*/error.json',
+        DATA_SOURCE = '<your_external_data_source>',
+        FORMAT = 'CSV',
+        FIELDTERMINATOR ='0x0b',
+        FIELDQUOTE = '0x0b',
+        ROWTERMINATOR = '0x0b'
+    ) WITH (doc NVARCHAR(MAX)) AS rowdata
+   CROSS APPLY openjson (doc)
+        WITH (  [Error] VARCHAR(1000) '$.Error',
+                [Row] INT '$.Row',
+                [Column] INT '$.Column',
+                ColumnName VARCHAR(1000) '$.ColumnName',
+                Value VARCHAR(1000) '$.Value',
+                [File] VARCHAR(1000) '$.File')
