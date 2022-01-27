f90d8682e3fb7f9d772f20baef9e16633a282609
datahai <70232001+datahai@users.noreply.github.com>
2022-01-27 09:04:08 +0000

    Create ObjectDependencies.sql

---
+++ dependencies/ObjectDependencies.sql
@@ -0,0 +1,13 @@
+--https://docs.microsoft.com/en-us/sql/relational-databases/views/get-information-about-a-view?view=sql-server-ver15--from 
+
+SELECT OBJECT_NAME(referencing_id) AS referencing_entity_name,   
+    o.type_desc AS referencing_desciption,   
+    COALESCE(COL_NAME(referencing_id, referencing_minor_id), '(n/a)') AS referencing_minor_id,   
+    referencing_class_desc, referenced_class_desc,  
+    referenced_server_name, referenced_database_name, referenced_schema_name,  
+    referenced_entity_name,   
+    COALESCE(COL_NAME(referenced_id, referenced_minor_id), '(n/a)') AS referenced_column_name,  
+    is_caller_dependent, is_ambiguous  
+FROM sys.sql_expression_dependencies AS sed  
+INNER JOIN sys.objects AS o ON sed.referencing_id = o.object_id  
+--WHERE referencing_id = OBJECT_ID(N'SCHEMA.OBJECTNAME');  
