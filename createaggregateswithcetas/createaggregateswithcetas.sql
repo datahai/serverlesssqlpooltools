c95828e6bfd04297e570d0144443ff9a2ab3b814
datahai <70232001+datahai@users.noreply.github.com>
2022-01-27 17:34:26 +0000

    Added parameters
    Added parameters for location, data source, and file format

--- createaggregateswithcetas/createaggregateswithcetas.sql
+++ createaggregateswithcetas/createaggregateswithcetas.sql
@@ -1,32 +1,38 @@
-CREATE PROCEDURE LDW.GeneratePreComputedDatasets @SourceView VARCHAR(100)
+CREATE PROCEDURE LDW.GeneratePreComputedDatasets 
+@SourceView NVARCHAR(200),
+@Location NVARCHAR(1000),
+@DataSource NVARCHAR(100),
+@FileFormat NVARCHAR(100)
 AS
 
 BEGIN
 
-DECLARE @Location VARCHAR(100),
-        @LocationTopLevel VARCHAR(100),
-        @ProcessDate CHAR(16),
+--declare variables,set the process date for the folder name, and set the locations
+DECLARE @LocationFull NVARCHAR(1100),
+        @LocationTopLevel NVARCHAR(1100),
+        @ProcessDate NCHAR(16),
         @SQLDrop NVARCHAR(2000),
         @CreateExternalTableString NVARCHAR(2000),
         @CreateCurrentAggregateView NVARCHAR(2000)
 
 SET @ProcessDate = FORMAT(GETDATE(),'yyyyMMddHHmmss')
 
-SET @SQLDrop = 
-'IF OBJECT_ID(''' + REPLACE(@SourceView,'.vw','') + '_PreComputeTable'') IS NOT NULL BEGIN DROP EXTERNAL TABLE ' + REPLACE(@SourceView,'.vw','') + '_PreComputeTable END'
+SET @LocationFull = CONCAT(@Location,REPLACE(@SourceView,'.',''),'/',@ProcessDate)
+SET @LocationTopLevel = CONCAT(@Location,REPLACE(@SourceView,'.',''))
+
+--Check for existence of an external table and drop if found
+SET @SQLDrop = 'IF OBJECT_ID(''' + REPLACE(@SourceView,'.vw','') + '_PreComputeTable'') IS NOT NULL BEGIN DROP EXTERNAL TABLE ' + REPLACE(@SourceView,'.vw','') + '_PreComputeTable END'
 
 EXEC sp_executesql @SQLDrop
 
-SET @Location = CONCAT('precomputeddatasets/dataminutes2/',REPLACE(@SourceView,'.',''),'/',@ProcessDate)
-SET @LocationTopLevel = CONCAT('precomputeddatasets/dataminutes2/',REPLACE(@SourceView,'.',''))
-
+--generate the SQL script to create the external table and export the View data
 SET @CreateExternalTableString = 
 'CREATE EXTERNAL TABLE ' + REPLACE(@SourceView,'.vw','') + '_PreComputeTable
 WITH 
 (
-  LOCATION = ''' + @location + ''',                                      
-  DATA_SOURCE = ExternalDataSourceDataLakeMI,
-  FILE_FORMAT = SynapseParquetFormat
+  LOCATION = ''' + @LocationFull + ''',                                      
+  DATA_SOURCE = ' + @DataSource + ',
+  FILE_FORMAT = ' + @FileFormat + '
 )
 AS
 SELECT 
@@ -35,16 +41,21 @@ FROM ' + @SourceView
 
 EXEC sp_executesql @CreateExternalTableString
 
+
+--drop the external table as we do not need it, it is only being used to generate the data.  We'll use a View to select the data
 SET @SQLDrop = 
 'IF OBJECT_ID(''' + REPLACE(@SourceView,'.vw','') + '_PreComputeTable'') IS NOT NULL BEGIN DROP EXTERNAL TABLE ' + REPLACE(@SourceView,'.vw','') + '_PreComputeTable END'
 
 EXEC sp_executesql @SQLDrop
 
+
+--drop the existing precompute View, we will re-create it
 SET @SQLDrop = 
 'IF OBJECT_ID(''' + @SourceView + '_PreComputeCurrent'') IS NOT NULL BEGIN DROP VIEW ' + @SourceView + '_PreComputeCurrent END'
 
 EXEC sp_executesql @SQLDrop
 
+--create a view to show the current data
 SET @CreateCurrentAggregateView = 
 'CREATE VIEW ' + @SourceView + '_PreComputeCurrent
 AS
@@ -56,7 +67,7 @@ AS
     OPENROWSET
     (
         BULK ''' + @LocationTopLevel + '/*/*.parquet'',     
-        DATA_SOURCE = ''ExternalDataSourceDataLakeMI'',
+        DATA_SOURCE = ''' + @DataSource + ''',
         FORMAT = ''Parquet''
     ) AS fct
 )
@@ -66,7 +77,7 @@ FROM
  OPENROWSET
 (
     BULK ''' + @LocationTopLevel + '/*/*.parquet'',     
-    DATA_SOURCE = ''ExternalDataSourceDataLakeMI'',
+    DATA_SOURCE = ''' + @DataSource + ''',
     FORMAT = ''Parquet''
 ) AS fct
 WHERE fct.filepath(1) IN (SELECT CurrentAggregates FROM CurrentFolder)'
